5e5a143c2488b60c7fba3a47ac27694d81df8388
Minwoo Bae <minwoobae@dyn-160-39-251-195.dyn.columbia.edu>
2017-01-28 13:19:46 -0500

    updated

--- divide_and_conquer/majority_element/majority_element.cpp
+++ divide_and_conquer/majority_element/majority_element.cpp
@@ -4,10 +4,33 @@
 
 using std::vector;
 
+/* Complie:
+g++ -pipe -O2 -std=c++11 majority_element.cpp -o majority_element
+g++ -pipe -O2 -std=c++14 majority_element.cpp -lm
+*/
+
+/*
+Majority Element
+*/
+
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
-  //write your code here
+  
+  sort(a.begin(), a.end());
+  int count = 1;
+  int element = -1;
+  for(int i = 0; i < a.size(); i++) {
+	  if(a[i] == element) {
+		  count++;
+	  }
+	  else{
+		  element = a[i];
+		  count = 1;
+	  }
+	  if(count > a.size()/2)
+		  return 1;
+  }
   return -1;
 }
 
